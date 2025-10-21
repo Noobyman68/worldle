aa4ad4b37ad66476e36ba4b45b8c49c5a27ffd62
Caleb Oglesby <noobyman68@gmail.com>
2025-10-20 23:40:00 -0400

    created txt_reader.c to read from sgb-word.txt and retrievd the word on each line of the file

---
+++ src/txt_reader.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define word_size 5
+
+int read_words(){
+  FILE *file;
+
+  if((file = fopen("..\\resources\\sgb-words.txt", "r")) == NULL){
+    printf("Error opening sgb-words file");
+    return -1;
+  }
+
+  char line[word_size];
+  
+  while((fgets(&line, word_size, file)) != EOF){
+    printf("%s\n", line);
+  }
+  return 0;
+}
+
+int main(){
+  int check;
+  if((check = read_words()) == -1){
+    printf("Error");
+  }
+  return 0;
+}
